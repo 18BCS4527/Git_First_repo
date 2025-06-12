eb191898fdf6db1ec660622f0fa8332f9bca711f
Chilakam Dinesh Reddy <45013767+18BCS4527@users.noreply.github.com>
2025-06-13 00:03:23 +0530

    Create newjava.java

---
+++ newjava.java
@@ -0,0 +1,27 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+
+@Configuration
+public class OAuthClientConfig {
+
+    @Bean
+    @ConfigurationProperties("cmp.oauth.client")
+    public ClientCredentialsResourceDetails cmpOAuthDetails() {
+        return new ClientCredentialsResourceDetails();
+    }
+
+    @Bean
+    @Qualifier("cmpRestTemplate")
+    public OAuth2RestTemplate cmpOAuthRestTemplate(ClientCredentialsResourceDetails details) {
+        OAuth2RestTemplate template = new OAuth2RestTemplate(details);
+        ClientCredentialsAccessTokenProvider provider = new ClientCredentialsAccessTokenProvider();
+        provider.setRequestEnhancer(new CmpAuthRequestTokenEnhancer());
+        template.setAccessTokenProvider(provider);
+        return template;
+    }
+}
