d5fc3db7300338469066d32b1239be827ce031b8
Chilakam Dinesh Reddy <45013767+18BCS4527@users.noreply.github.com>
2025-06-17 23:31:31 +0530

    Update newjava.java

--- newjava.java
+++ newjava.java
@@ -1,64 +1,44 @@
-class ServiceInterruption {
-    private NetworkMaintenanceServiceImpacts network_maintenance_service_impacts;
-    private Outages outages;
+class AccountIdentity {
+    private ChangeToAccount change_to_account;
+    private SecurityAlerts security_alerts;
 
     // Getters and Setters
-    public NetworkMaintenanceServiceImpacts getNetwork_maintenance_service_impacts() {
-        return network_maintenance_service_impacts;
+    public ChangeToAccount getChange_to_account() {
+        return change_to_account;
     }
-    public void setNetwork_maintenance_service_impacts(NetworkMaintenanceServiceImpacts network_maintenance_service_impacts) {
-        this.network_maintenance_service_impacts = network_maintenance_service_impacts;
+    public void setChange_to_account(ChangeToAccount change_to_account) {
+        this.change_to_account = change_to_account;
     }
 
-    public Outages getOutages() {
-        return outages;
+    public SecurityAlerts getSecurity_alerts() {
+        return security_alerts;
     }
-    public void setOutages(Outages outages) {
-        this.outages = outages;
+    public void setSecurity_alerts(SecurityAlerts security_alerts) {
+        this.security_alerts = security_alerts;
     }
 }
 
-class NetworkMaintenanceServiceImpacts {
-    private String email;
-    private String sms;
-    private String quiet_hours;
 
-    // Getters and Setters
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getSms() {
-        return sms;
-    }
-    public void setSms(String sms) {
-        this.sms = sms;
-    }
-    public String getQuiet_hours() {
-        return quiet_hours;
-    }
-    public void setQuiet_hours(String quiet_hours) {
-        this.quiet_hours = quiet_hours;
-    }
+class Billing {
+    private AutopayReminder autopay_reminder;
+    private PaymentConfirmation payment_confirmation;
+    private BillReady bill_ready;
+    private OnDemandPayPerViewPurchases on_demand_pay_per_view_purchases;
+    private XfinityRewards xfinity_rewards;
 }
 
-class Outages {
-    private String sms;
-    private String quiet_hours;
+class Appointment {
+    private TechnicianStatusUpdate technician_status_update;
+    private AppointmentReminderStatusSchedule appointment_reminder_status_schedule;
+}
 
-    // Getters and Setters
-    public String getSms() {
-        return sms;
-    }
-    public void setSms(String sms) {
-        this.sms = sms;
-    }
-    public String getQuiet_hours() {
-        return quiet_hours;
-    }
-    public void setQuiet_hours(String quiet_hours) {
-        this.quiet_hours = quiet_hours;
-    }
+
+class CustomerSupport {
+    private EquipmentNotifications equipment_notifications;
+    private TransferService transfer_service;
+    private ChangeOfPlan change_of_plan;
+}
+
+class Survey {
+    private CustomerSatisfactionSurvey customer_satisfaction_survey;
 }
